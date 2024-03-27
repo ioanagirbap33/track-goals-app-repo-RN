b3c6f88e56919bd67c16994eff99db2045b71eb5
ioana.girba <>
2024-03-27 14:15:39 +0200

    Initial layout

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hiiii</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal" />
+        <Button title="Add Goalll" />
+      </View>
+      <View>
+        <Text>List.....</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 100,
   },
 });
